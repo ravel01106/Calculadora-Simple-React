4ca6d5d480dadd8a981a0f73ea563732db556517
ravel01106 <ravello2002@gmail.com>
2023-09-23 11:08:50 +0100

    First commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import style from './Footer.module.css';
+
+const Footer = () => {
+    return (
+      <div>
+          <p className={`bg-secondary text-white ${style.Footer}`}>Proyecto 1 de react</p>
+      </div>
+    )
+  }
+  
+  
+  export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+        <h1 className="bg-secondary text-white pb-3 pt-1">Calculadora Simple</h1>
+    </div>
+  )
+}
+
+
+export default Header
